aa05bb4440de9f67b1f7a42f0cbafcf9bec701cf
Andi <familie.mayer@aon.at>
2019-08-25 18:38:00 +0200

    final version

--- src/app/checklist/checklist.page.ts
+++ src/app/checklist/checklist.page.ts
@@ -1,106 +1,106 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { AlertController, IonList } from '@ionic/angular';
-import { ChecklistDataService } from '../services/checklist-data.service';
-import { Checklist } from '../interfaces/checklists';
- 
+import {Component, ViewChild, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {AlertController, IonList} from '@ionic/angular';
+import {ChecklistDataService} from '../services/checklist-data.service';
+import {Checklist} from '../interfaces/checklists';
+
 @Component({
-  selector: 'app-checklist',
-  templateUrl: 'checklist.page.html',
-  styleUrls: ['checklist.page.scss'],
+    selector: 'app-checklist',
+    templateUrl: 'checklist.page.html',
+    styleUrls: ['checklist.page.scss'],
 })
 export class ChecklistPage implements OnInit {
 
-  @ViewChild(IonList) slidingList: IonList;
+    @ViewChild(IonList, {static: true}) slidingList: IonList;
 
-  private slug: string;
-  public checklist: Checklist;
+    private slug: string;
+    public checklist: Checklist;
 
-  constructor(private alertCtrl: AlertController, private route: ActivatedRoute, private dataService: ChecklistDataService){
+    constructor(private alertCtrl: AlertController, private route: ActivatedRoute, private dataService: ChecklistDataService) {
 
-  }
-
-  ngOnInit(){
-    this.slug = this.route.snapshot.paramMap.get('id');
-    this.loadChecklist();
-  }
-
-  loadChecklist(){
-    if(this.dataService.loaded){
-      this.checklist = this.dataService.getChecklist(this.slug);
-    } else {
-      this.dataService.load().then(() => {
-        this.checklist = this.dataService.getChecklist(this.slug);
-      });
     }
-  }
 
-  addItem(): void {
+    ngOnInit() {
+        this.slug = this.route.snapshot.paramMap.get('id');
+        this.loadChecklist();
+    }
 
-    this.alertCtrl.create({
-      header: 'Add Item',
-      message: 'Enter the name of the task for this checklist below:',
-      inputs: [
-        {
-          type: 'text',
-          name: 'name'
+    loadChecklist() {
+        if (this.dataService.loaded) {
+            this.checklist = this.dataService.getChecklist(this.slug);
+        } else {
+            this.dataService.load().then(() => {
+                this.checklist = this.dataService.getChecklist(this.slug);
+            });
         }
-      ],
-      buttons: [
-        {
-          text: 'Cancel'
-        },
-        {
-          text: 'Save',
-          handler: (data) => {
-            this.dataService.addItem(this.checklist.id, data);
-          }
-        }
-      ]
-    }).then((prompt) => {
-      prompt.present();
-    });
+    }
 
-  }
+    addItem(): void {
 
-  removeItem(item): void {
+        this.alertCtrl.create({
+            header: 'Add Item',
+            message: 'Enter the name of the task for this checklist below:',
+            inputs: [
+                {
+                    type: 'text',
+                    name: 'name'
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancel'
+                },
+                {
+                    text: 'Save',
+                    handler: (data) => {
+                        this.dataService.addItem(this.checklist.id, data);
+                    }
+                }
+            ]
+        }).then((prompt) => {
+            prompt.present();
+        });
 
-    this.slidingList.closeSlidingItems().then(() => {
-      this.dataService.removeItem(this.checklist, item);
-    });
+    }
 
-  }
+    removeItem(item): void {
 
-  renameItem(item): void {
+        this.slidingList.closeSlidingItems().then(() => {
+            this.dataService.removeItem(this.checklist, item);
+        });
 
-    this.alertCtrl.create({
-      header: 'Rename Item',
-      message: 'Enter the new name of the task for this checklist below:',
-      inputs: [
-        {
-          type: 'text',
-          name: 'name'
-        }
-      ],
-      buttons: [
-        {
-          text: 'Cancel'
-        },
-        {
-          text: 'Save',
-          handler: (data) => {
-            this.dataService.renameItem(item, data);
-          }
-        }
-      ]
-    }).then((prompt) => {
-      prompt.present();  
-    });
+    }
 
-  }
+    renameItem(item): void {
 
-  toggleItem(item): void {
-    this.dataService.toggleItem(item);  
-  }
+        this.alertCtrl.create({
+            header: 'Rename Item',
+            message: 'Enter the new name of the task for this checklist below:',
+            inputs: [
+                {
+                    type: 'text',
+                    name: 'name'
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancel'
+                },
+                {
+                    text: 'Save',
+                    handler: (data) => {
+                        this.dataService.renameItem(item, data);
+                    }
+                }
+            ]
+        }).then((prompt) => {
+            prompt.present();
+        });
+
+    }
+
+    toggleItem(item): void {
+        this.dataService.toggleItem(item);
+    }
 
 }
